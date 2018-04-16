ead1eb99e913053021e4cd8f2d2e424b9bcbff71
Rocky Chiang <rocky@chem.ucla.edu>
2018-04-16 21:20:48 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :author, only: [:show, :new, :edit, :create, :update]
-  resources :post, only: [:show, :new, :edit, :create, :update]
+  resources :authors, only: [:show, :new, :edit, :create, :update]
+  resources :posts, only: [:show, :new, :edit, :create, :update]
 
   # Example resource route with options:
   #   resources :products do
